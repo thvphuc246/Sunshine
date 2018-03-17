cce4ded70bdd79cc4edf7aab0dbe1ab73a4eafbe
thvphuc246 <truonghoangvinhphuc@gmail.com>
2018-03-17 19:50:31 +0200

    Prepare for Lifecycle part

--- app/src/main/java/com/example/vinhphuc/udacitysunshine/MainActivity.java
+++ app/src/main/java/com/example/vinhphuc/udacitysunshine/MainActivity.java
@@ -15,7 +15,6 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.ProgressBar;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.example.vinhphuc.udacitysunshine.ForecastAdapter.ForecastAdapterOnClickHandler;
 import com.example.vinhphuc.udacitysunshine.data.SunshinePreferences;
